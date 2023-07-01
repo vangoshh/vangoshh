09f00c412778a75dc00d0ac490e66989aa5c47b9
vangoshh <vangoshh@gmail.com>
2023-07-01 15:03:27 +0500

    fix auto test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,8 +5,8 @@ test('has title', async ({ page }) => {
   
   await page.goto('https://www.sima-land.ru/');
   await page.getByText('Войти').click()
-  await page.locator('xpath=/html/body/div[6]/div/div/div[2]/div[4]/div/div/div/form/div[2]/div/div/div/div/div/input').fill('qa_test@test.ru');
-  await page.locator('xpath=/html/body/div[6]/div/div/div[2]/div[4]/div/div/div/form/div[3]/div[1]/div/div[1]/div/div/input').fill('qa_test');
+  await page.locator('#entry-forms > div > div > div.os-host.os-host-foreign.os-theme-dark.os-host-resize-disabled.os-host-scrollbar-horizontal-hidden.UyGd8w.iHougY.os-host-flexbox.os-host-overflow.os-host-overflow-y.os-host-transition > div.os-padding > div > div > div > form > div.aLWWUx > div > div > div > div > div > input').fill('qa_test@test.ru');
+  await page.locator('#entry-forms > div > div > div.os-host.os-host-foreign.os-theme-dark.os-host-resize-disabled.os-host-scrollbar-horizontal-hidden.UyGd8w.iHougY.os-host-flexbox.os-host-overflow.os-host-overflow-y.os-host-transition > div.os-padding > div > div > div > form > div.cvONT1 > div.qmWmnS.owFhzM > div > div.JHtjBG > div > div > input').fill('qa_test');
   await page.getByRole('button', { name: 'Войти' }).click();
 
 
